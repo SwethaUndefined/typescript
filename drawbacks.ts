460261ba3e6021e6ed5a21ad375b026446f14633
“Swetha”Undefined <ramyaragu407@gmail.com>
2024-08-07 19:09:44 +0530

    first commit

---
+++ drawbacks.ts
@@ -0,0 +1,14 @@
+//Drawbacks
+
+
+//1. Compilation, until Compilation the browser doesnt understand ts code,
+
+// .ts   complies and convert the code into .js. This process is called Transpilation
+
+// Browsers do not understand TypeScript directly. They understand JavaScript.
+//  TypeScript is a superset of JavaScript that adds static typing and other features, 
+//  but it needs to be transpiled into plain JavaScript before it can be executed by a browser.
+
+
+//2. Discipline in coding
+
---
+++ whatIsTypeScript.ts
@@ -0,0 +1,31 @@
+//What is typescript?
+
+//Programming language created by microsoft, to address the some of the shortcomings in javascript.
+
+//Benifits:
+
+//1. Static typing
+//2. code completion
+//3. Refactoring
+//4. shorthand notations
+
+
+//statically typed language like,
+
+// int num = 110;
+// num = "q"
+
+
+//Not possible
+
+
+//Dynamically typed language like,
+
+// let num = 110;
+// num = "q"
+
+// possible
+
+let x:number = 10;
+console.log(x)
+//when i change the x to "a" throws an error
