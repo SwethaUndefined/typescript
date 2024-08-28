3842e1c3c3c38be53ba9273c3d35e77040be27a6
“Swetha”Undefined <ramyaragu407@gmail.com>
2024-08-28 17:53:33 +0530

    commit 1

---
+++ configuringTheTypescriptCompiler/index.ts
@@ -0,0 +1,7 @@
+// target in tsConfig.json file is tells which version of js that the ts compiler is going to generate
+console.log(10)
+
+let age : number = 20;
+console.log(age)
+if(age < 50)
+ age  = age +10;    
---
+++ hello-world/index.ts
@@ -0,0 +1,7 @@
+console.log("Hello world")
+
+
+let age : number = 20;
+// age = "a"  Type 'string' is not assignable to type 'number'.
+
+console.log(age)
